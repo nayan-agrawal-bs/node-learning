973f95501f1140f871ea28d03c7719e30cc2b668
BigStep <nayan.agrawal@bigsteptech.com>
2023-01-25 01:48:00 +0530

    First Commit

---
+++ crud.js
@@ -0,0 +1,80 @@
+var fs = require("fs");
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const readLineAsync = (msg) => {
+  return new Promise((resolve) => {
+    readline.question(msg, (userRes) => {
+      resolve(userRes);
+    });
+  });
+};
+
+const startApp = async () => {
+  const userRes = await readLineAsync(
+    "Please select an option \n 1) Create a entry \n 2) Read \n 3) Update \n 4) Delete \n"
+  );
+  switch (userRes) {
+    case "1":
+      let id = await readLineAsync("Input Id\n");
+      let name = await readLineAsync("Input Name\n");
+      fs.readFile("input.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        let arr = JSON.parse(data);
+        arr.push({ id, name });
+        fs.writeFile("input.json", JSON.stringify(arr), (err) => {
+          if (err) throw err;
+        });
+      });
+      break;
+    case "2":
+      fs.readFile("input.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        let arr = JSON.parse(data);
+        console.log(arr);
+      });
+      break;
+    case "3":
+      let updateId = await readLineAsync("Input Id\n");
+      let updatedName = await readLineAsync("Input Name\n");
+      fs.readFile("input.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        let arr = JSON.parse(data);
+        arr.forEach((element) => {
+          if (element.id == updateId) {
+            element.name = updatedName;
+          }
+        });
+        fs.writeFile("input.json", JSON.stringify(arr), (err) => {
+          if (err) throw err;
+        });
+      });
+      break;
+    case "4":
+      let deleteId = await readLineAsync("Input Id\n");
+      let index;
+      fs.readFile("input.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        let arr = JSON.parse(data);
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i].id === deleteId) {
+            index = i;
+            break;
+          }
+        }
+        arr.splice(index, 1);
+        fs.writeFile("input.json", JSON.stringify(arr), (err) => {
+          if (err) throw err;
+        });
+      });
+      break;
+    default:
+      console.log("wrong input");
+  }
+  readline.close();
+};
+
+startApp();
--- first.js
+++ first.js
@@ -1,53 +1,39 @@
 const fs = require("fs");
 let data = [
     {
-      firstname: 'ABC',
-      lastname: 'DEF',
+      firstname: 'YOU',
+      lastname: 'EEF',
       runs: 2000,
-      iplTeam: 'Mumbai',
+      iplTeam: 'Kolkata',
       wickets: 5,
-      matchPlayed: 20,
-    },
-    {
-        firstname: 'NYA',
-        lastname: 'YO',
-        runs: 3000,
-        iplTeam: 'Chennai',
-        wickets: 6,
-        matchPlayed: 24,
-      },
-      {
-        firstname: 'KBC',
-        lastname: 'ROC',
-        runs: 5000,
-        iplTeam: 'Rajasthan',
-        wickets: 3,
-        matchPlayed: 22,
-      }
+      matchPlayed: 27,
+    }
   ]
 
+const file = fs.readFileSync('input.json',"utf-8")
+console.log(file);
+//Missiing step convert the buffer to an array
+const json = JSON.parse(file.toString())
+//add json element to json object
+json.push(data);
 fs.writeFileSync("input.json", JSON.stringify(data));
-
-const data2 = fs.readFileSync('input.json',
-            {encoding:'utf8', flag:'r'});
-console.log(data2);
 console.log('-----------------------');
 
-//async
-fs.writeFile("data.json",JSON.stringify(data), (err)=> {
-    console.log('Complete');
-});
+// async
 fs.readFile('data.json',"utf-8",(err,data)=>{
     console.log(data);
     console.log("read");
 })
-
-fs.open("data1.json","w+", function(err,h){
-    //Sync
-    fs.write(h,JSON.stringify(data),(err) => {
-        console.log('Success');
-    });
+fs.writeFile("data.json",JSON.stringify(data), (err)=> {
+    console.log('Complete');
 });
 
 
+fs.open("data1.json","w+", function(err,h){
+    // //Sync
+    fs.write(h,JSON.stringify(data),(err) => {
+        console.log('Success');
+    });
+    console.log(err,"\\\\",h);
+});
 
